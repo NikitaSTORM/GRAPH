99d52729a407e816a9e9bedab4c5c39d8b648f81
storm <33227564+NikitaSTORM@users.noreply.github.com>
2019-03-26 22:35:32 +0700

    Add files via upload

---
+++ GRAPH/GRAF/GRAF/Form1.Designer.cs
@@ -0,0 +1,219 @@
+﻿namespace GRAF
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.Windows.Forms.DataVisualization.Charting.ChartArea chartArea1 = new System.Windows.Forms.DataVisualization.Charting.ChartArea();
+            System.Windows.Forms.DataVisualization.Charting.Legend legend1 = new System.Windows.Forms.DataVisualization.Charting.Legend();
+            System.Windows.Forms.DataVisualization.Charting.Series series1 = new System.Windows.Forms.DataVisualization.Charting.Series();
+            this.chart1 = new System.Windows.Forms.DataVisualization.Charting.Chart();
+            this.buttonCalc = new System.Windows.Forms.Button();
+            this.textBoxAmpl0 = new System.Windows.Forms.TextBox();
+            this.textBoxXmax = new System.Windows.Forms.TextBox();
+            this.textBoxXmin = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.textBoxChatSobstColeb = new System.Windows.Forms.TextBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.textBoxAmplV = new System.Windows.Forms.TextBox();
+            this.textBoxChatV = new System.Windows.Forms.TextBox();
+            this.label6 = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.chart1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // chart1
+            // 
+            chartArea1.Name = "ChartArea1";
+            this.chart1.ChartAreas.Add(chartArea1);
+            legend1.Name = "Legend1";
+            this.chart1.Legends.Add(legend1);
+            this.chart1.Location = new System.Drawing.Point(427, 0);
+            this.chart1.Name = "chart1";
+            series1.ChartArea = "ChartArea1";
+            series1.ChartType = System.Windows.Forms.DataVisualization.Charting.SeriesChartType.Spline;
+            series1.Legend = "Legend1";
+            series1.Name = "Series1";
+            this.chart1.Series.Add(series1);
+            this.chart1.Size = new System.Drawing.Size(458, 536);
+            this.chart1.TabIndex = 0;
+            this.chart1.Text = "chart1";
+            // 
+            // buttonCalc
+            // 
+            this.buttonCalc.Location = new System.Drawing.Point(311, 46);
+            this.buttonCalc.Name = "buttonCalc";
+            this.buttonCalc.Size = new System.Drawing.Size(75, 23);
+            this.buttonCalc.TabIndex = 1;
+            this.buttonCalc.Text = "RASCHET";
+            this.buttonCalc.UseVisualStyleBackColor = true;
+            this.buttonCalc.Click += new System.EventHandler(this.buttonCalc_Click);
+            // 
+            // textBoxAmpl0
+            // 
+            this.textBoxAmpl0.Location = new System.Drawing.Point(66, 158);
+            this.textBoxAmpl0.Name = "textBoxAmpl0";
+            this.textBoxAmpl0.Size = new System.Drawing.Size(167, 20);
+            this.textBoxAmpl0.TabIndex = 2;
+            // 
+            // textBoxXmax
+            // 
+            this.textBoxXmax.Location = new System.Drawing.Point(66, 197);
+            this.textBoxXmax.Name = "textBoxXmax";
+            this.textBoxXmax.Size = new System.Drawing.Size(167, 20);
+            this.textBoxXmax.TabIndex = 3;
+            // 
+            // textBoxXmin
+            // 
+            this.textBoxXmin.Location = new System.Drawing.Point(69, 236);
+            this.textBoxXmin.Name = "textBoxXmin";
+            this.textBoxXmin.Size = new System.Drawing.Size(167, 20);
+            this.textBoxXmin.TabIndex = 4;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(63, 142);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(170, 13);
+            this.label1.TabIndex = 5;
+            this.label1.Text = "Начальня амплитуда колебаний";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(69, 181);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(33, 13);
+            this.label2.TabIndex = 6;
+            this.label2.Text = "Xmax";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(69, 220);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(30, 13);
+            this.label3.TabIndex = 7;
+            this.label3.Text = "Xmin";
+            // 
+            // textBoxChatSobstColeb
+            // 
+            this.textBoxChatSobstColeb.Location = new System.Drawing.Point(66, 278);
+            this.textBoxChatSobstColeb.Name = "textBoxChatSobstColeb";
+            this.textBoxChatSobstColeb.Size = new System.Drawing.Size(167, 20);
+            this.textBoxChatSobstColeb.TabIndex = 8;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(66, 259);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(172, 13);
+            this.label4.TabIndex = 9;
+            this.label4.Text = "частота собственных колебаний";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(66, 330);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(167, 13);
+            this.label5.TabIndex = 10;
+            this.label5.Text = "амплитуда вынуждающей силы";
+            // 
+            // textBoxAmplV
+            // 
+            this.textBoxAmplV.Location = new System.Drawing.Point(69, 347);
+            this.textBoxAmplV.Name = "textBoxAmplV";
+            this.textBoxAmplV.Size = new System.Drawing.Size(164, 20);
+            this.textBoxAmplV.TabIndex = 11;
+            // 
+            // textBoxChatV
+            // 
+            this.textBoxChatV.Location = new System.Drawing.Point(66, 408);
+            this.textBoxChatV.Name = "textBoxChatV";
+            this.textBoxChatV.Size = new System.Drawing.Size(167, 20);
+            this.textBoxChatV.TabIndex = 12;
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(66, 389);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(179, 13);
+            this.label6.TabIndex = 13;
+            this.label6.Text = "частота вынуждающих колебаний";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(897, 576);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.textBoxChatV);
+            this.Controls.Add(this.textBoxAmplV);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.textBoxChatSobstColeb);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.textBoxXmin);
+            this.Controls.Add(this.textBoxXmax);
+            this.Controls.Add(this.textBoxAmpl0);
+            this.Controls.Add(this.buttonCalc);
+            this.Controls.Add(this.chart1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.chart1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.DataVisualization.Charting.Chart chart1;
+        private System.Windows.Forms.Button buttonCalc;
+        private System.Windows.Forms.TextBox textBoxAmpl0;
+        private System.Windows.Forms.TextBox textBoxXmax;
+        private System.Windows.Forms.TextBox textBoxXmin;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox textBoxChatSobstColeb;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.TextBox textBoxAmplV;
+        private System.Windows.Forms.TextBox textBoxChatV;
+        private System.Windows.Forms.Label label6;
+    }
+}
+
---
+++ GRAPH/GRAF/GRAF/Form1.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace GRAF
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void buttonCalc_Click(object sender, EventArgs e)
+        {
+            double Xmin = double.Parse(textBoxXmin.Text);
+            double Xmax = double.Parse(textBoxXmax.Text);
+            double Ampl = double.Parse(textBoxAmpl0.Text);
+            double ChastSobstColeb = double.Parse(textBoxChatSobstColeb.Text);
+            double AmplV = double.Parse(textBoxAmplV.Text);
+            double chastV = double.Parse(textBoxChatV.Text); 
+            int count = (int)Math.Ceiling((Xmax - Xmin) *Math.Max(chastV,ChastSobstColeb)) + 1;
+            double[] x = new double[count];
+            double[] y1 = new double[count];
+            double[] y2 = new double[count];
+            for (int i = 0; i < count; i++)
+            {
+                x[i] = Xmin +  (Xmax-Xmin) * i/ (count - 1);
+                y1[i] = Ampl*Math.Cos(ChastSobstColeb*x[i])+AmplV/(Math.Pow(ChastSobstColeb,2)-Math.Pow(chastV, 2))*Math.Cos(chastV*x[i]);
+                //y2[i] = Math.Cos(x[i]);
+            }
+            chart1.ChartAreas[0].AxisX.Minimum = Xmin;
+            chart1.ChartAreas[0].AxisX.Maximum = Xmax;
+
+            //chart1.ChartAreas[0].AxisX.MajorGrid.Interval = ;
+
+            chart1.Series[0].Points.DataBindXY(x, y1);
+            //chart1.Series[0].Points.DataBindXY(x, y2);
+
+        }
+    }
+}
